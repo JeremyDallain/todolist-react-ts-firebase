73eed6abca20655447e9394318efd76dfde7f534
JeremyDallain <jdallain87@gmail.com>
2023-05-02 09:41:14 +0200

    add sticky footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { addTodo, getTodos, updateTodo, deleteTodo } from "./services/firebase";
 import { TodoCounter } from "./components/TodoCounter";
 import { AddTodo } from "./components/AddTodo";
 import TodoList from "./components/TodoList";
+import { Footer } from "./components/Footer";
 
 export interface Todo {
   id: string;
@@ -35,15 +36,20 @@ const App = () => {
   };
 
   return (
-    <div className="max-w-lg w-full p-6 m-auto mt-20">
-      <h1 className="text-3xl font-bold text-center text-gray-800 p-2 mb-8">Todos</h1>
-      <AddTodo createTodo={createTodo} />
-      <TodoList
-        todos={todos}
-        toggleComplete={toggleComplete}
-        removeTodo={removeTodo}
-      />
-      <TodoCounter count={todos.length} />
+    <div className="flex flex-col min-h-screen max-w-lg w-full m-auto pt-12">
+      <div className="flex-grow">
+        <h1 className="text-3xl font-bold text-center text-gray-800 p-2 mb-8">
+          Todos
+        </h1>
+        <AddTodo createTodo={createTodo} />
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+        />
+        <TodoCounter count={todos.length} />
+      </div>
+      <Footer />
     </div>
   );
 };
